782c145019945e5572d41bca39048535f41d0b56
AdhamBasheir <adhambasheir000@gmail.com>
2025-03-05 09:26:15 +0200

    added lower() in Question number 9
    fixed an issue where the script does not run as intended in mixed case

--- day2.py
+++ day2.py
@@ -16,7 +16,6 @@ tuple1 = (1, 2, 3)
 tuple2 = (2, 3, 4)
 print(set(tuple1) & set(tuple2))
 
-
 # 6.Find the Maximum and Minimum Value in a Dictionary
 my_dict = {"a": 10, "b": 20, "c": 5} 
 print(max(my_dict.values()), min(my_dict.values()))
@@ -27,7 +26,6 @@ dict2 = {"c": 3, "d": 4}
 dict1.update(dict2)
 print(dict1)
 
-
 # 8- Find Common Keys in Two Dictionaries
 dict1 = {"a": 1, "b": 2, "c": 3}
 dict2 = {"b": 2, "c": 4, "d": 5}
@@ -35,4 +33,4 @@ print(set(dict1) & set(dict2))
 
 # 9- takes a string and prints the longest
 s = input("Enter a string: ")
-print(max([s[:i+1] for i in range(len(s)) if s[:i+1] == ''.join(sorted(s[:i+1]))], key=len))
+print(max([s[:i+1] for i in range(len(s)) if s[:i+1].lower() == ''.join(sorted(s[:i+1].lower()))], key=len))
